e976d9d233043614f97e02ee3da8ec11961a262c
vanvy102 <38358643+vanvy102@users.noreply.github.com>
2023-04-09 01:48:28 +0700

    Update time_1.py

--- time_1.py
+++ time_1.py
@@ -1,17 +1,26 @@
-import requests
-import json
-import streamlit as st
-
-url = "https://fiin-core.ssi.com.vn/Master/GetListOrganization?language=vi"
-
-try:
-    response = requests.get(url)
-    data = json.loads(response.text)
-    st.write('good')
-except requests.exceptions.JSONDecodeError:
-    st.error("Lỗi giải mã dữ liệu JSON")
-    data = None
-except Exception as e:
-    st.error("Đã xảy ra lỗi: " + str(e))
-data = None
-print(data)
+from vnstock import*
+import pandas as pd
+import os 
+import datetime
+import config as cf
+#lay moc thoi gian giao dich trong 30 ngay
+current_time= datetime.date.today()
+thirty_days_ago= current_time+ datetime.timedelta(-30)
+#os.makedirs('nganhang', exist_ok=True) 
+start_time=str(thirty_days_ago)
+end_time=str(current_time)
+#lay thong tin cua cac ngan hang
+#nh=cf.get_ticket()
+ticket=[]
+with open('MaNH.txt','r') as f:
+    h=f.read()
+    h=h.splitlines()
+    for i in h:
+        ticket.append(i)
+print(ticket)
+#lấy thông tin giao dich của từng ngân hàng
+for ti in ticket :
+    df = stock_historical_data(symbol=ti, 
+                            start_date=start_time, 
+                            end_date=end_time)
+st.write(f"{df}")
