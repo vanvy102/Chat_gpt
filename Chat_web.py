8d20c153cf92db01222b4fab19b1509b35c4849a
vanvy102 <38358643+vanvy102@users.noreply.github.com>
2023-05-07 18:49:33 +0700

    Update Chat_web.py

--- Chat_web.py
+++ Chat_web.py
@@ -7,11 +7,13 @@ from streamlit_chat import message
 import crawl_link as cr
 # Cài đặt thông tin model
 api_gpt=st.secrets["api_GPT"]
+token=st.secrets["token"]
+shift=st.secrets["shift"]
 openai.api_key=api_gpt
 #Hàm để gọi đến OpenAPI / Phần ChatGPT
 def get_response_from_chatgpt(user_question):
     response = openai.Completion.create(
-        engine= "text-davinci-003",
+        engine= config.model,
         prompt = user_question,
         max_tokens = 3000,
         n = 1,
@@ -20,11 +22,9 @@ def get_response_from_chatgpt(user_question):
     response_text = response.choices[0].text
     return response_text
 ticket=['...']
-with open('MaNH.txt','r') as f:
-    h=f.read()
-    h=h.splitlines()
-    for i in h:
-        ticket.append(i)
+tk=config.get_ticket()
+for i in tk:
+    ticket.append(i)
 # #Container1:
 with st.container():
    st.markdown("<h1 style='text-align: center; color: grey;'>SMART TRADING GPT</h1>", unsafe_allow_html=True)
@@ -33,7 +33,7 @@ with st.container():
 with st.container():
     string_chat ='chào bạn!'
     #chia container 2 thành 2 cột: 30% - 70%
-    column1,column2=st.columns([0.3,0.7]);
+    column1,column2=st.columns([0.3,0.7])
     # Hàng 2: Tab và bảng trống
     def clear_session():
         st.session_state.clear()
@@ -44,39 +44,67 @@ with st.container():
         if selected_stock=='...' or buuttons:
             clear_session()
             st.stop()
-        
-        with column2:
+        Y_train=linear.linear_data(selected_stock,token,shift)
+
+        # questions=[f'với những thông tin và giá mở phiên {int(Y_train[0])} thì mã cổ phiếu này có ưu thế gì?']
+        # selected_question=st.selectbox('câu hỏi:',questions)
+        # print(selected_question)
+        # if 'bot' not in st.session_state:
+        #     st.session_state['bot']=[]
+        # if 'user' not in st.session_state:
+        #     st.session_state['user']=[]
+        # response_text=get_response_from_chatgpt(selected_question)
+        # st.session_state.bot.append(response_text)
+        # st.session_state.user.append(selected_question)
+    with column2:
         #tao ban tom tat
-            container1=st.container()
-            container2=st.container()
-            container3=st.container()
+        container1=st.container()
+        container2=st.container()
+        container3=st.container()
+        if 'bot' not in st.session_state:
+            st.session_state['bot']=[]
+        if 'user' not in st.session_state:
+            st.session_state['user']=[]
+        with container3:
+            q2="với những thông tin và giá mở phiên "+ str(Y_train[0]) +" thì mã cổ phiếu này có ưu thế gì?"
+            selected_question=st.selectbox('câu hỏi:',('với những thông tin đó có những rủi ro nào?',q2))
+            print(selected_question)
             #du lieu du doan
             #tao thu vien chat
-            if 'bot' not in st.session_state:
-                st.session_state['bot']=[]
-            if 'user' not in st.session_state:
-                st.session_state['user']=[]
-            smr='tóm tắt bài viết '
+        # if 'bot' not in st.session_state:
+        #     st.session_state['bot']=[]
+        # if 'user' not in st.session_state:
+        #     st.session_state['user']=[]
+            current_time= datetime.date.today()
+            current_year=current_time.year
+            smr='tóm tắt bài viết, dữ liệu cho năm '+ str(current_year)
             data=cr.get_link_cafef(selected_stock)
-            Y_train=linear.linear_data(selected_stock)
+            for i in range(len(data)):
+                data[i]=smr +' '+data[i]
+            print(data)
             summary=get_response_from_chatgpt(data[0])
+            print(summary)
             summary1=get_response_from_chatgpt(data[1])
+            print(summary1)
             summary2=get_response_from_chatgpt(data[2])
-            total_sum=get_response_from_chatgpt('Kết hợp ba bài viết trên thành một bài viết tóm tắt')
+            print(summary2)
+            total_sum=summary+'\n'+summary1+'\n'+summary2
+        #total_sum=get_response_from_chatgpt('Kết hợp ba bài viết trên thành một bài viết')
             container1.write(f"{total_sum}")
-            user_question = container3.text_input("Nhập câu hỏi vào đây:")          
-            if container3.button("Chat với em đi")or user_question:
-                response_text = get_response_from_chatgpt(user_question)
-                st.session_state.bot.append(response_text)
-                st.session_state.user.append(user_question)
+        
+            # st.session_state.bot.append(response_text)
+            # st.session_state.user.append(user_question)
+        
+            response_text=get_response_from_chatgpt(selected_question)
+            st.session_state.bot.append(response_text)
+            st.session_state.user.append(selected_question)
             with container2:
-                message('Chào bạn!')
                 if st.session_state['user']:
                     for i in range(len(st.session_state['user'])):
                         message(st.session_state["user"][i],is_user=True, key=str(i)+'_user')
                         if i == len(st.session_state['user'])-1:
                             with st.spinner('Đang xử lý...'):
                                 time.sleep(1)
-                        message(st.session_state['bot'][i], key=str(i))    
+                        message(st.session_state['bot'][i], key=str(i))   
      
     
