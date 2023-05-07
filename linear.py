185c3e193a398f1e72569cb51dd997a752320113
vanvy102 <38358643+vanvy102@users.noreply.github.com>
2023-05-07 20:53:56 +0700

    Update linear.py

--- linear.py
+++ linear.py
@@ -5,11 +5,7 @@ import csv
 from vnstock import*
 import config as cf
 import random
-def linear_data(ticket):
-#cp=listing_companies()
-# nh=cf.get_ticket()
-# ti=['ACB','ABB']
-# for ticket in ti:
+def linear_data(ticket,token, shift):
     data=[]
     df=cf.get_data_bank(ticket)
     for k in range(len(df)):
@@ -27,23 +23,24 @@ def linear_data(ticket):
     K=[]
     h=[]
     for i in range(len(data)):
-        K.append([float(data[i][1]),float(data[i][2])])
-        h.append(float(data[i][0]))
+        K.append([int(data[i][1]),int(data[i][2])])
+        h.append(int(data[i][0]))
     # Tạo mô hình hồi quy tuyến tính
     model = LinearRegression()
     x_mean=np.mean(K,axis=0)
     y_mean=np.mean(h)
     # Huấn luyện mô hình với dữ liệu
     model.fit(K,h)
-    r_sq = model.score(K, h)
+    #r_sq = model.score(K, h)
     #print('coefficient of determination:', r_sq)
     # In ra các hệ số của mô hình
     #print('Coefficients:', model.coef_)
     # Dự đoán giá trị mới
-    temp=random.randint(0,len(data)-1)
+    
     #print(temp)
-    x_new = np.array([K[temp]])
+    X_train=gsd.get_data(ticket,token,shift)
+    x_new = [X_train[len(X_train)-1]]
     #print(x_new)
     y_new = model.predict(x_new)
     #print('Predicted value:', y_new)
-    return [y_new[0],y_mean]
+    return [int(y_new[0]),int(y_mean)]
